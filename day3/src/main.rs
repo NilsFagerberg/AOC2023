4514b410c5662ca423f20d8e6e1f795ddb8bc712
Nils Fagerberg <fagerberg.nils@gmail.com>
2023-12-06 21:38:44 +0100

    Add solution for day 3 part 1 and start day 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,53 +11,38 @@ fn day3_part1(file_path: &str) -> Result<usize, Error> {
 
     // line e.g. "......832....@..........*.951*....984*111..801"
     for (i, line) in file.lines().enumerate() {
-        let splits = line.split('.').filter(|s|!s.is_empty());
 
         let mut potential_parts_line: BTreeMap<usize, String> = BTreeMap::new();
         let mut symbols_line: BTreeMap<usize, String> = BTreeMap::new();
-        
-        // split e.g. "832" or "984*111"
-        // TODO: rewrite this shit without the indices
-        for split in splits {
-            let mut found_digits = String::new();
 
-            for c in split.chars() {
-                if c.is_ascii_digit() {
-                    found_digits.push(c);
-                } else {
-                    if !found_digits.is_empty() {
-                        let num_str = found_digits.as_str();
-                        for found in line.match_indices(num_str) {
-                            if (potential_parts_line.contains_key(&found.0)) {
-                                if (num_str.len() > found.1.len()) {
-                                    potential_parts_line.insert(found.0, num_str.to_string());
-                                }
-                            } else {
-                                potential_parts_line.insert(found.0, found.1.to_string());
-                            }
-                        }
-                        found_digits.clear();
-                    }
-                    for found in line.match_indices(c) {
-                        symbols_line.insert(found.0, found.1.to_string());
-                    }
+        let mut found_digits = String::new();
+        let mut digit_ix = 0;
+
+        for (j, c) in line.chars().enumerate() {
+            if (c.is_ascii_digit()) {
+                if (found_digits.is_empty()) {
+                    digit_ix = j;
                 }
+                found_digits.push(c);
+                continue;
             }
-            if !found_digits.is_empty() {
-                let num_str = found_digits.as_str();
-                for found in line.match_indices(num_str) {
-                    if (potential_parts_line.contains_key(&found.0)) {
-                        if (num_str.len() > found.1.len()) {
-                            potential_parts_line.insert(found.0, num_str.to_string());
-                        }
-                    } else {
-                        potential_parts_line.insert(found.0, found.1.to_string());
-                    }
-                }
+            if (!found_digits.is_empty()) {
+                potential_parts_line.insert(digit_ix, found_digits.clone());
+                digit_ix = 0;
+                found_digits.clear();
             }
+            if (c == '.') {
+                continue;
+            }
+            symbols_line.insert(j, c.to_string());
+
+        }
+        if (!found_digits.is_empty()) {
+            potential_parts_line.insert(digit_ix, found_digits.clone());
+            digit_ix = 0;
             found_digits.clear();
         }
-
+        
         potential_parts.push(potential_parts_line);
         symbols.push(symbols_line);
 
@@ -111,6 +96,66 @@ fn day3_part1(file_path: &str) -> Result<usize, Error> {
     Ok(sum)
 }
 
+fn day3_part2(file_path: &str) -> Result<usize, Error> {
+
+    let file = std::fs::read_to_string(file_path)?;
+
+    let mut sum = 0;
+
+    let mut potential_parts: Vec<BTreeMap<usize, String>> = Vec::new();
+    let mut symbols: Vec<BTreeMap<usize, String>> = Vec::new();
+
+    // line e.g. "......832....@..........*.951*....984*111..801"
+    for (i, line) in file.lines().enumerate() {
+
+        let mut potential_parts_line: BTreeMap<usize, String> = BTreeMap::new();
+        let mut symbols_line: BTreeMap<usize, String> = BTreeMap::new();
+
+        let mut found_digits = String::new();
+        let mut digit_ix = 0;
+
+        for (j, c) in line.chars().enumerate() {
+            if (c.is_ascii_digit()) {
+                if (found_digits.is_empty()) {
+                    digit_ix = j;
+                }
+                found_digits.push(c);
+                continue;
+            }
+            if (!found_digits.is_empty()) {
+                potential_parts_line.insert(digit_ix, found_digits.clone());
+                digit_ix = 0;
+                found_digits.clear();
+            }
+            if (c == '.') {
+                continue;
+            }
+            if (c == '*') {
+                symbols_line.insert(j, c.to_string());
+            }
+
+        }
+        if (!found_digits.is_empty()) {
+            potential_parts_line.insert(digit_ix, found_digits.clone());
+            digit_ix = 0;
+            found_digits.clear();
+        }
+        
+        potential_parts.push(potential_parts_line);
+        symbols.push(symbols_line);
+
+    } // Parsing done
+
+    let lines: Vec<&str> = file.lines().collect();
+
+    for (i, gears_per_line) in symbols.iter().enumerate() {
+        for gear in gears_per_line {
+            
+        }
+    } 
+    Ok(2)
+}
+
 fn main() {
     // Part 1
     let res_part1 = day3_part1("day3/input/actual.txt").expect("An error occurred!");
@@ -130,12 +175,11 @@ fn test_part1() {
     assert_eq!(res.unwrap(), 4361);
 }
 
-/*
+
 #[test]
 fn test_part2() {
-    let res = day2_part2("day2/input/test.txt");
+    let res = day3_part2("day3/input/test.txt");
 
     assert!(res.is_ok());
-    assert_eq!(res.unwrap(), 2286);
+    assert_eq!(res.unwrap(), 467835);
 }
-*/
